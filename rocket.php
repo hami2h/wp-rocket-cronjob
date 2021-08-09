5ab0b7b02baa561c56c52680beac1e86ec66aa09
Hamed Heydari <ruzerastakhiz@gmail.com>
2021-08-09 14:02:04 +0430

    Add files via upload

---
+++ rocket.php
@@ -0,0 +1,33 @@
+<?php
+
+// Load WordPress.
+$folder_path = "/home/cfannip7/public_html/wp-content/cache/min/1/"; 
+
+$files = glob($folder_path.'/*');  
+   
+
+foreach($files as $file) { 
+   
+    if(is_file($file))  
+    
+        // Delete the given file 
+        unlink($file);  
+}
+
+require( 'wp-load.php' );
+
+// Clear cache.
+
+if ( function_exists( 'rocket_clean_domain' ) ) {
+
+                rocket_clean_domain();
+
+}
+
+// Preload cache.
+
+if ( function_exists( 'run_rocket_sitemap_preload' ) ) {
+
+                run_rocket_sitemap_preload();
+
+}
